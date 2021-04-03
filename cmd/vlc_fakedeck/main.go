dfd918ca6e15fd604c937f971a8c3fce145dba60
Kim "BKC" Carlbäcker <kim.carlbacker@gmail.com>
2021-04-03 20:23:18 +0200

    Set logger to info

--- cmd/vlc_fakedeck/main.go
+++ cmd/vlc_fakedeck/main.go
@@ -1,6 +1,14 @@
 package main
 
+import (
+	"os"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
 func main() {
+	log.Logger = zerolog.New(os.Stdout).Level(zerolog.InfoLevel).With().Timestamp().Logger()
 	d := VLCDeckNew()
 	d.PowerOn()
 }
